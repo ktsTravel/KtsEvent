fa9d0b900a3cbcd115ef3decc7ea6c0e08003d93
AzureAD\FadyChebly <fady.c@sirenassociates.com>
2022-02-09 19:14:10 +0200

    added static files locations in middlewar and created general layout file'

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const app = express()
 const path = require('path')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override');
+const expressLayouts = require('express-ejs-layouts')
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
+app.set('layout', './Layouts/index')
 
+//static files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/javascript', express.static(__dirname + 'public/javascript'))
+
+
+app.use(expressLayouts)
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
 
 app.get('/', (req, res) => {
-	// res.send('Ahlaaaaaaa')
 	res.render('Home/LandingPage')
 })
 
